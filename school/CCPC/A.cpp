7a5f9b6c21147d69aa6ec02fe38c49c128500e8d
LingNc <1020449099@qq.com>
2024-05-02 16:01:39 +0800

    sync

---
+++ school/CCPC/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define foe(i,a,b) for(int i=a;i<=b;i++)
+#define endl '\n'
+
+using namespace std;
+
+void pri(){
+    
+}
+int main(){
+    
+    return 0;
+}
