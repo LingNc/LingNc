ee4242ef2f7fbca5571da40dac2b59d396b9cbe7
LingNc <1020449099@qq.com>
2024-05-02 22:52:30 +0800

    sync

---
+++ daily/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char num[50]={ 0 };
+    scanf("%s",num);
+    int len=strlen(num);
+    int m=len%3;
+    for(int i=0;i<=len-1;i++){
+        if((i-m)%3==0&&i!=0)
+            printf("%c",',');
+        printf("%c",num[i]);
+    }
+    return 0;
+}
---
+++ daily/1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+int main(){
+    std::string num;
+    std::cin>>num;
+    
+    return 0;
+}
