5470762a910b93112c6287d01820291aec885ccc
LingNc <1020449099@qq.com>
2024-07-22 11:01:56 +0800

    不会八数码

--- school/暑假集训/acwing/843n-皇后问题2.cpp
+++ school/暑假集训/acwing/843n-皇后问题2.cpp
@@ -10,22 +10,45 @@ const int N=10;
 int n;
 char map[N][N];
 bool row[N],col[N],dg[N],udg[N];
-void dfs(int x,int y){
-    if(x==n&&y<n) y++,x=0;
-    if(x==n&&y==n){
-        foe(i,0,n-1) puts(map[i]);
+// 深度优先搜索函数，用于解决八皇后问题
+// 参数:
+// x, y: 当前放置皇后的列数和行数
+// Q: 已放置的皇后数量
+void dfs(int x, int y, int Q)
+{
+    // 当到达最后一列时，行数增加，列数重置为0，准备放置下一行的皇后
+    if (x == n) 
+        y++, x = 0;
+    
+    // 如果已经放置了n个皇后，解决方案找到
+    if (y == n) {
+        // 如果放置了n个皇后，打印解决方案
+        if (Q == n) {
+            for (int i = 0; i < n; i++) 
+                puts(map[i]);
+            cout << endl;
+        }
+        // 返回，结束当前路径的探索
         return;
     }
-    //no set
-    dfs(x+1,y);
+    
+    // 尝试在下一列不放置皇后，继续搜索
+    // 不放皇后
+    dfs(x + 1, y, Q);
 
-    //set
-    if(!row[y]&&!col[x]&&!dg[x+y]&&!udg[n+y-x]){
-        map[y][x]='Q';
-        row[y]=col[x]=dg[x+y]=udg[n+y-x]=true;
-        dfs(0,y+1);
-        map[y][x]='.';
-        row[y]=col[x]=dg[x+y]=udg[n+y-x]=false;
+    // 尝试在当前列放置皇后
+    // 放皇后
+    if (!row[y] && !col[x] && !dg[x + y] && !udg[n + y - x]) {
+        // 在当前位置放置皇后，更新相应位置的标记
+        map[y][x] = 'Q';
+        row[y] = col[x] = dg[x + y] = udg[n + y - x] = true;
+        
+        // 继续搜索下一列，放置下一个皇后
+        dfs(x + 1, y, Q + 1);
+        
+        // 回溯，撤销当前位置的皇后，恢复标记
+        map[y][x] = '.';
+        row[y] = col[x] = dg[x + y] = udg[n + y - x] = false;
     }
 }
 int main(){
@@ -33,6 +56,6 @@ int main(){
     foe(i,0,n-1)foe(j,0,n-1){
         map[i][j]='.';
     }
-    dfs(0,0);
+    dfs(0,0,0);
     return 0;
 }
---
+++ school/暑假集训/acwing/844.走迷宫-路径.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<utility>
+#define ll long long
+#define foe(i,a,b) for(int i=a;i<=b;i++)
+#define endl '\n'
+
+using namespace std;
+
+const int N=110;
+
+int n,m;
+int map[N][N];
+int d[N][N];
+
+//模拟队列
+typedef pair<int,int> Pr;
+Pr q[N*N],prv[N][N];
+
+//最短路径
+int bfs(){
+    int head=0,tail=0;
+    q[0]={0,0};
+    memset(d,-1,sizeof(d));
+    d[0][0]=0;
+
+    int dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};
+    while(head<=tail){
+        auto t=q[head++];
+        foe(i,0,3){
+            int x=t.first+dx[i],y=t.second+dy[i];
+            if(x>=0&&x<n&&y>=0&&y<m&&map[x][y]==0&&d[x][y]==-1){
+                d[x][y]=d[t.first][t.second]+1;
+                q[++tail]={x,y};
+                prv[x][y]=t;
+            }
+        }
+    }
+    //打印路径
+    int x=n-1,y=m-1;
+    while(x||y){
+        cout<<x<<' '<<y<<endl;
+        auto t=prv[x][y];
+        x=t.first,y=t.second;
+    }
+    return d[n-1][m-1];
+}
+int main(){
+    cin>>n>>m;
+    foe(i,0,n-1)
+        foe(j,0,m-1){
+        cin>>map[i][j];
+    }
+    cout<<bfs()<<endl;
+    return 0;
+}
