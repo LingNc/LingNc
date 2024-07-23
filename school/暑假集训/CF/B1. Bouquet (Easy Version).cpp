2f4f8785bd1e94ac3f899c8e75e0b92f62309581
LingNc <1020449099@qq.com>
2024-07-24 01:21:28 +0800

    第一场codeforce 434

--- school/暑假集训/CF/B1. Bouquet (Easy Version).cpp
+++ school/暑假集训/CF/B1. Bouquet (Easy Version).cpp
@@ -10,11 +10,66 @@ using namespace std;
 int solve(int n,int m){
     priority_queue<int> q;
     int num;
-    foe(i,1,m){
+    foe(i,1,n){
         cin>>num;
         q.push(num);
     }
+    int sum=0,f=0,last=-1,start=-1;
+    int max_res=-1;
+    while(q.size()){
+        int t=q.top();
+        q.pop();
 
+        if(t<=m){
+            if(start-t==0){
+                sum+=t;
+                if(sum>m){
+                    sum-=t;
+                    max_res=max(max_res,sum);
+                    //new
+                    sum=t;
+                    start=t;
+                }
+            }
+            else if(start-t==1){
+                if(last-t==1){
+                    f++;
+                    if(f==2){
+                        f=0;
+                        max_res=max(max_res,sum);
+                        sum=t;
+                        start=t;
+                    }
+                    else{
+                        sum+=t;
+                        if(sum>m){
+                            sum-=1+t;
+                        }
+                        if(sum==m) max_res=max(max_res,sum);
+                    }
+                }
+                else if(last-t==0){
+                    sum+=t;
+                    if(sum>m){
+                        sum-=1+t;
+                    }
+                    if(sum==m) max_res=max(max_res,sum);
+                }
+            }
+            else if(start-t>1){
+                f=0;
+                max_res=max(max_res,sum);
+                sum=t;
+                start=t;
+            }
+            else if(start-t<0){
+                sum+=t;
+                start=t;
+            }
+            last=t;
+        }
+    }
+    return max(max_res,sum);
 }
 signed main(){
     ios::sync_with_stdio(0),cin.tie(0);
