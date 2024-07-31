0bf35c1fb5678a3ee2d469600873ad56f1f41e23
LingNc <1020449099@qq.com>
2024-07-31 23:18:43 +0800

    sync

--- school/暑假集训/萌新联赛（三）/D-开心消消乐.cpp
+++ school/暑假集训/萌新联赛（三）/D-开心消消乐.cpp
@@ -1,33 +1,40 @@
 #include<bits/stdc++.h>
+#define ll long long 
 #define foe(i,a,b) for(int i=(a);i<=(b);i++)
 #define fod(i,a,b) for(int i=(a);i>=(b);i--)
-#define int long long
 #define endl '\n'
 #define IOS ios::sync_with_stdio(0),cin.tie(0)
-
-using namespace std;
+#define all(a) a.begin(),a.end()
 using i32=signed;
-const int N=10;
+using namespace std;
+
+const int N=1e6+10;
+int q[N];
+int hh,tt;
 
 void solve(){
     int n;
     cin>>n;
-    string a,b;
-    cin>>a>>b;
+    hh=0,tt=0;
+    q[0]=-1;
+    foe(i,0,n-1) cin>>q[++tt];
+    int t2=tt;
     int ans=0;
-    foe(i,0,n-3){
-        if(a.substr(i,3)=="x.x"&&
-            b.substr(i,3)=="..."||
-            a.substr(i,3)=="..."&&
-            b.substr(i,3)=="x.x")
+    while(tt>=hh){
+        int t=q[tt--];
+        if(t==0) continue;
+        if(t!=q[t2]||tt<0){
             ans++;
-
+            t2=tt+1;
+        }
     }
     cout<<ans<<endl;
 }
+
 i32 main(){
+    IOS;
     i32 T=1;
-    cin>>T;
+    //cin>>T;
     while(T--) solve();
     return 0;
 }
