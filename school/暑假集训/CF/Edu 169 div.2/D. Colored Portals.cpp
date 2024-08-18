91387bf70cd9afe00b61c816e62cc1b50c6759f7
LingNc <1020449099@qq.com>
2024-08-18 23:46:15 +0800

    sync

--- school/暑假集训/CF/Edu 169 div.2/D. Colored Portals.cpp
+++ school/暑假集训/CF/Edu 169 div.2/D. Colored Portals.cpp
@@ -15,42 +15,62 @@ using namespace std;
 
 const i32 N=0;
 
+bool eq(string &a,string &b){
+    int cnt=0;
+    for(auto &i:a)
+        for(auto &j:b)
+            if(i==j) cnt++;
+    return (!cnt)?0:1;
+}
+int lower(vector<int> &str,int val){
+    int l=-1,r=str.size();
+    while(l+1<r){
+        int mid=l+(r-l>>1);
+        if(str[mid]>=val) r=mid;
+        else l=mid;
+    }
+    return str[r];
+}
+int upper(vector<int> &str,int val){
+    int l=-1,r=str.size();
+    while(l+1<r){
+        int mid=l+(r-l>>1);
+        if(str[mid]>=val) r=mid;
+        else l=mid;
+    }
+    return str[l];
+}
 void solve(){
     int n,q;
     cin>>n>>q;
     vector<string> a(n+1);
-    map<string,int> mp;
+    unordered_map<string,vector<int>> mp;
     foe(i,1,n){
         cin>>a[i];
-        mp[a[i]]++;
+        mp[a[i]].push_back(i);
+    }
+    for(auto &it:mp){
+        sort(all(it.second),less<int>());
     }
     int x,y;
     foe(i,1,q){
         cin>>x>>y;
         int l=min(x,y),r=max(x,y);
-        if(mp.size()<=2||a[l]==a[r]){
-            if(a[l]==a[r]) cout<<abs(r-l)<<endl;
+        if(mp.size()<=2||eq(a[l],a[r])){
+            if(eq(a[l],a[r])) cout<<abs(r-l)<<endl;
             else cout<<-1<<endl;
             continue;
         }
-        bool res=0;
-        foe(i,l,r) if(a[i]!=a[l]&&a[i]!=a[r]){
-            res=1;
-            break;
+        int ll=-1,rr=INF32;
+        for(auto &it:mp){
+            if(it.first==a[l]||it.first==a[r]) continue;
+            ll=max(ll,lower(it.second,l));
+            rr=min(rr,upper(it.second,l));
         }
-        if(res) cout<<abs(r-l)<<endl;
-        else{
-            int s1,s2;
-            foe(i,r,n) if(a[i]!=a[l]&&a[i]!=a[r]){
-                s1=abs(l-i)+abs(r-i);
-                break;
-            }
-            fod(i,r,1) if(a[i]!=a[l]&&a[i]!=a[r]){
-                s2=abs(l-i)+abs(r-i);
-                break;
-            }
-            cout<<min(s1,s2)<<endl;
-        }
+        int s1=INF32,s2=INF32;
+        if(ll!=-1) s1=abs(l-ll)+abs(r-ll);
+        if(rr!=INF32) s2=abs(l-rr)+abs(r-rr);
+        cout<<min(s1,s2)<<endl;
     }
 }
 i32 main(){
