d54727e9ce123b960feb69e33cbf3e9755590092
LingNc <1020449099@qq.com>
2025-05-13 17:42:53 +0800

    新增huffcode模块，包括huffcode.h和huffcode.c文件，实现哈夫曼编码的创建、初始化、打印、获取和设置功能，并支持位移操作；更新lab2.code-workspace以包含新的文件类型关联。

---
+++ school/数据结构实验/lab2/src/huffcode.c
@@ -0,0 +1,49 @@
+#include "huffcode.h"
+
+huffcode new_huffcode(){
+    huffcode res = malloc(sizeof(HuffCode));
+    if (res == NULL) return NULL;
+    res->_code = 0;
+    res->_size = 0;
+    return res;
+}
+huffcode huffcode_init(huffcode self){
+    if(self==NULL) return NULL;
+    self->_code=0;
+    self->_size=0;
+    return self;
+}
+
+void huffcode_print(huffcode self){
+    if (self == NULL) return;
+    printf("HuffCode: %X %u\n", self->_code, self->_size);
+}
+bool huffcode_get(huffcode self,Byte index){
+    // 超出索引范围
+    if(index>64) return false;
+    index=64-self->_size+index;
+    return (self->_code>>index)&1;
+}
+any huffcode_set(huffcode self,Byte index,bool value){
+    if(self==NULL||index>64) return NULL;
+    index=64-self->_size+index;
+    self->_code=(self->_code&~(1<<index))|(value<<index);
+    return self;
+}
+void huffcode_inc(huffcode self){
+    if(self==NULL) return NULL;
+    self->_code+=1;
+}
+void huffcode_lshift(huffcode self,size_t nums){
+    if(self==NULL) return NULL;
+    self->_code<<=nums;
+    self->_size+=nums;
+}
+void huffcode_rshift(huffcode self,size_t nums){
+    if(self==NULL) return NULL;
+    self->_code>>=nums;
+    self->_size-=nums;
+}
+interface huffcode_create_inter(){
+    return new_interface(sizeof(HuffCode), NULL, "ip",huffcode_init,huffcode_print);
+}
